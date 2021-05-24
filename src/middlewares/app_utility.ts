ee1e5780f546c7d705e695b0fa08286f3f39a5ca
Otagera <Otagera@gmail.com>
2021-05-24 09:51:13 +0100

    Init

---
+++ src/middlewares/app_utility.ts
@@ -0,0 +1,56 @@
+import * as http from 'http';
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+export const normalizePort = (val: any) => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) { return val; }
+
+  if (port >= 0) { return port; }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+export const onError = (error: any, port: string) => {
+  if (error.syscall !== 'listen') { throw error; }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+export const onListening = (server: http.Server, port: string) => {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : (addr)? 'port ' + addr.port: '';
+  debug('Listening on ' + bind);
+  console.log("=============");
+  console.log("App is listening from port: " + port);
+  console.log("=============");
+}
